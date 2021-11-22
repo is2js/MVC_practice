47e1a2df674377be6bbca83c56b3d9a60da3d4d6
is2js <tingstyle1@gmail.com>
2021-11-22 14:43:12 +0900

    feat(view): 등록과 취소 메서드 추가

--- src/main/java/trade/Application.java
+++ src/main/java/trade/Application.java
@@ -10,26 +10,36 @@ import trade.domain.Register;
 import trade.view.InputView;
 import trade.view.OutputView;
 
+import static trade.view.OutputView.printItem;
+
 public class Application {
     public static void main(String[] args) {
         //1.main에 일단 몰아서 한다고 치면, class도 여기 내부에서 만들어줘도 된다.
         // -> 일단은 나누서 작성해보려고함.
 
         //2. 컨트롤러로 옮길 예정인데, 테스트도 여기서 해보자.
-        Item item = new Item("오버워치", 340);
+//        Item item = new Item("오버워치", 340);
         Register register = new Register();
 
         //3. 안내문들은 OutputView의 메서드로 다 빼논다.
-        OutputView outputView = new OutputView();
-        outputView.printTitle();
+        // -> 객체를 굳이 이용할필요도, 상태(private)도 없으니, static메소드로 정해서 사용하자.
+//        OutputView outputView = new OutputView();
+        OutputView.printTitle();
         //4. InputView에선 static scanner객체를 건네주는 유틸메소드를 이용해서 string만 넘겨받는다.
-        outputView.printNameInstruction();
+        OutputView.printNameInstruction();
         String itemName = InputView.getInput();
-        outputView.printMoneyInstruction();
+
+        OutputView.printMoneyInstruction();
         int itemMoney = Integer.parseInt(InputView.getInput());
-        outputView.printRegisterInstruction();
-        String answer = InputView.getInput();
-        outputView.printRegisterResult(answer);
+
+        Item item = new Item(itemName, itemMoney);
+
+        // 5. 등록된 Item(사용자마자 달라지는 부분)을 받아서, print하는 것도
+        // -> [domain, model] Item 클래스가 아닌, [View] OutputView의 static메소드로 정의하자.
+        OutputView.printItem(item);
+        OutputView.printRegisterInstruction();
+        String answerRegister = InputView.getInput();
+        OutputView.printRegisterItemInstruction(answerRegister);
 
 
 
--- src/main/java/trade/view/OutputView.java
+++ src/main/java/trade/view/OutputView.java
@@ -1,29 +1,38 @@
 package trade.view;
 
+import trade.domain.Item;
+
 import java.util.Locale;
 
 public class OutputView {
 
-    public void printTitle() {
+    public static void printTitle() {
         System.out.println("중고나라에 오신 것을 환영합니다.");
     }
-    public void printNameInstruction() {
+    public static void printNameInstruction() {
         System.out.println("등록할 물품명을 입력해주세요.");
     }
-    public void printMoneyInstruction() {
+    public static void printMoneyInstruction() {
         System.out.println("등록할 금액을 입력해주세요.");
     }
-    public void printRegisterInstruction() {
+    public static void printRegisterInstruction() {
         System.out.println("해당 아이템을 해당 금액에 등록하시겠습니까? (Y, N)");
     }
 
-    public void printRegisterResult(String answer) {
-        if (answer.equalsIgnoreCase("y")) {
+    public static void printItem(Item item){
+        System.out.println(item.getName() + " 제품의 가격은 " + item.getMoney() +"원 입니다. ");
+    }
+
+    public static void printRegisterItemInstruction(String answerRegister) {
+
+        if (answerRegister.equalsIgnoreCase("y")) {
             System.out.println("정상 등록 되었습니다.");
+            //TODO: 실제 등록
             return;
         }
-        if (answer.equalsIgnoreCase("n")) {
+        if (answerRegister.equalsIgnoreCase("n")) {
             System.out.println("등록 취소 되었습니다.");
+            //TODO: 실제 등록 취소
             return;
         }
         System.out.println("y나 n을 입력해주세요.");
