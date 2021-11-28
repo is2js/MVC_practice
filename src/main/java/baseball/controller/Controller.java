8955800b7588d12ebc6d6ccdecc86f8948b3a6a7
is2js <tingstyle1@gmail.com>
2021-11-28 15:29:52 +0900

    basebasell1: 기능구현목록 작성

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,4 +1,12 @@
 package baseball.controller;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Controller {
+	public void run() {
+		OutputView.printInstruction();
+		String input = InputView.getInput();
+		System.out.println(input);
+	}
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,10 @@
 package baseball.view;
 
 public class OutputView {
+
+	public static final String INPUT_NUMBER_INSTRUCTION = "숫자를 입력해주세요 : ";
+
+	public static void printInstruction() {
+		System.out.print(INPUT_NUMBER_INSTRUCTION);
+	}
 }
